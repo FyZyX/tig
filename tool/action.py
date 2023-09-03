172e975281e70c89c6e36f66ed02e9a03de84ecc
FyZyX <lucasmlofaro@gmail.com>
2023-09-03 08:51:37 -0700

    improve tool stubs

--- tool/action.py
+++ tool/action.py
@@ -1,29 +1,63 @@
-def list_modules():
+from typing import Optional
+
+
+# Lists all available modules in the codebase
+def list_modules() -> list[str]:
     pass
 
 
-def get_module():
+# Gets a specific module's details
+def get_module(module_name: str) -> dict[str, str | list[str]]:
     pass
 
 
-def get_class():
+# Gets a specific class's details from a module
+def get_class(
+        module_name: str,
+        class_name: str,
+) -> dict[str, str | list[str]]:
     pass
 
 
-def get_function():
-    pass
-
-def add_module():
-    pass
-
-
-def add_class():
-    pass
-
-
-def add_function():
+# Gets a specific function's details from a module or class
+def get_function(
+        module_name: str,
+        class_name: Optional[str],
+        function_name: str,
+) -> dict[str, str]:
     pass
 
 
-def update_function(module_name: str, class_name: str = None):
+# Adds a new module to the codebase
+def add_module(
+        module_name: str,
+) -> bool:
+    pass
+
+
+# Adds a new class to a module
+def add_class(
+        module_name: str, class_name: str,
+        content: str,
+) -> bool:
+    pass
+
+
+# Adds a new function to a module or class
+def add_function(
+        module_name: str,
+        class_name: str | None,
+        function_name: str,
+        content: str,
+) -> bool:
+    pass
+
+
+# Updates an existing function in a module or class
+def update_function(
+        module_name: str,
+        class_name: str | None,
+        function_name: str,
+        new_content: str,
+) -> bool:
     pass
