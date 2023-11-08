012727942bc245f769431c2c32bdcfec5e426430
FyZyX <lucasmlofaro@gmail.com>
2023-11-08 00:54:24 -0800

    add path exclusions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import fnmatch
 
 
-def project_structure(root, indent='', depth=1):
+def project_structure(root, indent='', depth=1, excludes=None):
     root_path = Path(root).expanduser()
 
     if not root_path.is_dir():
@@ -11,15 +12,20 @@ def project_structure(root, indent='', depth=1):
     if depth <= 0:
         return
 
+    excludes = excludes or []
+
     depth -= 1
 
     for entry in sorted(root_path.iterdir(), key=lambda e: e.name):
+        if any(fnmatch.fnmatch(entry.name, pattern) for pattern in excludes):
+            continue
+
         print(f"{indent}{entry.name}")
         if entry.is_dir():
-            project_structure(entry, indent + '    ', depth)
+            project_structure(entry, indent + '    ', depth, excludes)
 
 
 if __name__ == '__main__':
     codebase = "~/Projects/github/justin-time"
-    # Set the depth to whatever level of recursion you want, e.g., 2 levels deep
-    project_structure(codebase, depth=2)
+    excludes = ["*.git", "__pycache__", "*.pyc", ".idea", "venv"]
+    project_structure(codebase, depth=2, excludes=excludes)
